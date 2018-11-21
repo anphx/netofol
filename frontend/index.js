e3bb7f0a63085c640fb5dbe5357f0ee6b7177512
An Pham <ptan1991@gmail.com>
2018-11-21 16:23:18 +0100

    - Added jquey

---
+++ frontend/index.js
@@ -0,0 +1,37 @@
+const QUESTION_MAP = [{
+        ind: 1,
+        q: "In which sector of activity is your company active? (linked with your APE code)",
+        a: [
+            "Industry",
+            "Business",
+            "Information and communication",
+            "Other services activity",
+            "All other activities (financial, agricultural activities ...)",
+            "Construction",
+            "Public sector",
+            "Specialized, Scientific and Technical or Administrative and Support Services Activities",
+            "Real estate"
+        ],
+        topic: "",
+        qType: QUESTION_TYPE.SINGLE_CHOICE
+    },
+    {
+        q: "aba",
+        a: ["ahafk", ""],
+        topic: ""
+    }
+];
+
+const QUESTION_TYPE = {
+    SINGLE_CHOICE: 0,
+    MULTI_CHOICE: 1,
+}
+
+const TOPICS
+
+function test() {
+    // body...
+    console.log("abcs");
+}
+
+test();
