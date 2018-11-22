2004eae092d4cc4f16e0531647d33b03c2b9e16d
anisul <anis.2803@gmail.com>
2018-11-22 01:12:01 +0100

    refactored

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
+import pymongo
+from pymongo import MongoClient
 from flask import Flask, request
 from flask import make_response, render_template
-from flask_pymongo import PyMongo
+
 
 import json, uuid
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/d4g"
-mongo = PyMongo(app)
-
-
+MONGO_HOST = "mongodb://anis:tillicollapse4411@ds133353.mlab.com:33353/d4g"
+MONGO_PORT = 23456
+MONGO_DB = "d4g"
+MONGO_USER = "anis"
+MONGO_PASS = "tillicollapse"
+connection = MongoClient(MONGO_HOST, MONGO_PORT)
+mongo = connection[MONGO_DB]
 
 
 @app.route("/")
@@ -19,7 +24,7 @@ def main():
 
 @app.route("/s/<session_id>", methods=['GET'])
 def load_with_session(session_id):
-    return render_template('index.html', session = session_id, history = load_history(session_id))
+    return render_template('index.html', session = session_id, history = list(load_history(session_id)))
 
 
 @app.route('/save', methods=['POST'])
@@ -27,7 +32,7 @@ def save():
     session_id = request.json['session_id']
     history = request.json['history']
 
-    if session_id == 0:
+    if session_id == '0':
         session_id = str(uuid.uuid4())[:8]
 
     data = {}
@@ -40,7 +45,7 @@ def save():
 
 def load_history(session):
     # load history from db
-    res = mongo.d4g.findOne({"session_id":session});
+    res = mongo.d4g.find({"session_id":session});
     return res
 
 def save_to_db(session_id, history):
